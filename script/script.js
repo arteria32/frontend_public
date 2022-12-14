85d4c20f930c16331278eb711c0c45e937bf8c15
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2022-12-14 13:14:45 +0300

    Making task 2.1-2.5

---
+++ script/script.js
@@ -0,0 +1,15 @@
+//Задание 2.1 (2 способ)
+// alert("Я JavaScript!")
+
+//Задание 2.4 (Работа с переменными)
+let admin, name;
+name = "Джон";
+admin = name;
+alert(admin)
+
+//Задание 2.4 (Придумайте правильные имена)
+let nameOurPlanet = "Земля";
+let currentUserOnline = "Алексей";
+
+//Задание 2.6 (Взаимодействие: alert, prompt, confirm)
+
