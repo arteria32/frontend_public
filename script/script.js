e2a2b6469d0420a909533807dbe4ba518953510e
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2022-12-15 15:45:40 +0300

    Making 2.6-2.8

--- script/script.js
+++ script/script.js
@@ -2,14 +2,21 @@
 // alert("Я JavaScript!")
 
 //Задание 2.4 (Работа с переменными)
-let admin, name;
-name = "Джон";
-admin = name;
-alert(admin)
+// let admin, name;
+// name = "Джон";
+// admin = name;
+// alert(admin)
 
 //Задание 2.4 (Придумайте правильные имена)
 let nameOurPlanet = "Земля";
 let currentUserOnline = "Алексей";
 
-//Задание 2.6 (Взаимодействие: alert, prompt, confirm)
+//Задание 2.6 (Простая страница)  
+// let name = prompt("Как тебя зовут");
+// alert(`Приветствую, ${name}!`)
+
+//Задание 2.8 (Исправьте сложение)  
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+// alert(+a + +b);
 
