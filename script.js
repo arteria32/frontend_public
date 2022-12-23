6502a3de631edf1621f8916a14c07a753296eae1
Osoboe <Osoboe_mnenie1@mail.ru>
2022-12-23 23:49:50 +0300

    DZ_6_VASILEV

--- script.js
+++ script.js
@@ -1,231 +1,136 @@
-// 2.1 Привет мир
+// Задание 1. Поиск всех уникальных значений в массиве
+// Тестовые данные:
+// [1, 2, 4, 2, 4, 4, 5]
+// [1, 2, 3, 4, 5]
+// [1, -2, -2, 3, 4, -5, -6, -5]
+// Ожидаемый результат:
+// [1,2,4,5]
+// [1,2,3,4,5]
+// [1,-2,3,4,-5,-6]
 
-// alert( "Я JAVASCRIPT" );
-
-// 2.4 Переменные
-
-// Задача 1
-// let admin, name;
-// name = "Джон";
-// admin = name;
-// alert( admin );
-
-// Задача 2
-// let ourPlanetName = "Земля";
-// let currentUserName = "Вася";
-
-// 2.5 Типы данных
-
-// let name = "Ilya";
-// alert( `hello ${1}` ); // hello 1
-// alert( `hello ${"name"}` ); // hello name
-// alert( `hello ${name}` ); // hello Ilya
-
-// 2.6 Взаимодействие: alert, prompt, confirm
-
-// let currentUserName = prompt("Ваше имя?", "");
-// alert(currentUserName);
-
-// 2.8 Базовые операторы, математика
-
-// Задача 1
-// let a = 1, b = 1;
-// alert( ++a ); // 2
-// alert( b++ ); // 1
-
-// Задача 3
-// "" + 1 + 0 // "10"
-// "" - 1 + 0 // -1
-// true + false // 1
-// 6 / "3" // 2
-// "2" * "3" // 6
-// 4 + 5 + "px" // "9px"
-// "$" + 4 + 5 // "$45"
-// "4" - 2 // 2
-// "4px" - 2 // NaN
-// "  -9  " + 5 // "  -9  5"
-// "  -9  " - 5 // -14
-// null + 1 // 1
-// undefined + 1 // NaN
-// " \t \n" - 2 // -2
-
-// Задача 4
-// let a = +prompt("Первое число?", 1);
-// let b = +prompt("Второе число?", 2);
-// alert(a + b);
-
-// 2.9 Операторы сравнения
-
-// 5 > 4 // true
-// "ананас" > "яблоко" // false
-// "2" > "12" // true
-// undefined == null // true
-// undefined === null // false
-// null === +"\n0\n" // false
-
-//2.10 Условное ветвление
-
-// Задача 1
-// let value = prompt('Какое "официальное" название JavaScript?', '');
-// if (value == 'ECMAScript') {
-//   alert('Верно!');
-// } else {
-//   alert('Не знаете? ECMAScript!');
+// function search(arr) {
+//   return Array.from(new Set(arr));
 // }
+// console.log(search([1, -2, -2, 3, 4, -5, -6, -5]));
 
-// Задача 2
-// let value = prompt('Введите число', 0);
-// if (value > 0) {
-//   alert(1);
-// } else if (value < 0) {
-//   alert(-1);
-// } else {
-//   alert(0);
-// }
+// Задание 2. Определить сколько вмещается слов в
+// предложение
+// На входе мы мы получаем количество символов в предложении и массив из слов. Необходимо
+// вывести количество слов, которое точно поместится в предложение.
+// Тестовые данные:
+// limesToCut(25, ['small', 'small', 'large', 'medium', 'small']);
+// Ожидаемый результат:
+// 4
 
-// Задача 3
-// result = (a + b < 4) ? 'Мало' : 'Много';
-
-// Задача 4
-// let message =
-//   login == 'Сотрудник'
-//     ? 'Привет'
-//     : login == 'Директор'
-//     ? 'Здравствуйте'
-//     : login == ''
-//     ? 'Нет логина'
-//     : '';
-
-// 2.11 Логические операторы
-
-// Задача 1
-// if (age >= 14 && age <= 90)
-
-// Задача 2
-// if (age < 14 || age > 90)
-
-// Задача 3
-// let userName = prompt('Кто там?', '');
-// if (userName === 'Админ') {
-//   let pass = prompt('Пароль?', '');
-//   if (pass === 'Я главный') {
-//     alert('Здравствуйте!');
-//   } else if (pass === '' || pass === null) {
-//     alert('Отменено');
-//   } else {
-//     alert('Неверный пароль');
+// function limesToCut(amount, arr) {
+//   let count = 0;
+//   for (word of arr) {
+//     if (amount >= word.length) {
+//       amount = amount - word.length;
+//       count++;
+//     }
 //   }
-// } else if (userName === '' || userName === null) {
-//   alert('Отменено');
-// } else {
-//   alert('Я вас не знаю');
+//   return count;
 // }
+// console.log(limesToCut(30, ['small', 'small', 'large', 'medium', 'small']));
 
-// 2.13 Циклы while и for
+// Задание 3. Определить количество нечетных чисел в
+// массиве
+// Тестовые данные:
+// [1, 2, 3, 1, 5, 6],
+// Ожидаемый результат:
+// 4
 
-// Задача 1
-// for (let i = 2; i <= 10; i++) {
-//   if (i % 2 == 0) {
-//     alert(i);
+// function countOdd(arr) {
+//   let count = 0;
+//   for (num of arr) {
+//     if (num % 2 == 1) {
+//       count++;
+//     }
 //   }
+//   return count;
 // }
+// console.log(countOdd([1, 2, 3, 1, 5, 6, 6, 1]));
 
-// Задача 2
-// let i = 0;
-// while (i < 3) {
-//   alert(`number ${i}!`);
-//   i++;
-// }
+// Задание 4. Преобразить порядковый номер игральной карты
+// в ее роль
+// Тестовые данные:
+// [5, 11,12,13, 10]
+// Ожидаемый результат:
+// [5, 'jack', 'queen', 'king', 10]
 
-// Задача 3
-// let num;
-// do {
-//   num = prompt('Введите число больше 100?', 0);
-// } while (num <= 100 && num);
-
-// Задача 4
-// let n = 10;
-// nextPrime: for (let i = 2; i <= n; i++) {
-//   for (let j = 2; j < i; j++) {
-//     if (i % j == 0) continue nextPrime;
+// function convertCard(arr) {
+//   for (i in arr) {
+//     if (arr[i] == 11) {
+//       arr.splice(i, 1, 'jack');
+//     } else if (arr[i] == 12) {
+//       arr.splice(i, 1, 'queen');
+//     } else if (arr[i] == 13) {
+//       arr.splice(i, 1, 'king');
+//     }
 //   }
-//   alert(i);
+//   return arr;
 // }
+// console.log(convertCard([5, 11, 12, 13, 10]));
 
-// 2.14 Конструкция "switch"
+// Задание 5. Создание визитки
+// Тестовые данные:
+// createVisitor(‘Иван Иванович',, 24, '89005553535');
+// Ожидаемый результат:
+//  { name: ‘Иван Иванович', age: 24, number: '89005553535' }
 
-// Задача 1
-// if (browser == 'Edge') {
-//   alert("You've got the Edge!");
-// } else if (
-//   browser == 'Chrome' ||
-//   browser == 'Firefox' ||
-//   browser == 'Safari' ||
-//   browser == 'Opera'
-// ) {
-//   alert('Okay we support these browsers too');
-// } else {
-//   alert('We hope that this page looks ok!');
+// function createVisitor(name, age, number) {
+//   return { name, age, number };
 // }
+// console.log(createVisitor('Иван Иванович', 24, '89005553535'));
 
-// Задача 2
-// const number = +prompt('Введите число между 0 и 3', '');
-// switch (number) {
-//   case 0:
-//     alert('Вы ввели число 0');
-//     break;
+// Задание 6. Удалить минимум
+// Тестовые данные:
+// [1,2,3,4,5]
+// Ожидаемый результат:
+// [2,3,4,5]
 
-//   case 1:
-//     alert('Вы ввели число 1');
-//     break;
-
-//   case 2:
-//   case 3:
-//     alert('Вы ввели число 2, а может и 3');
-//     break;
-// }
-
-// 2.15 Функции
-
-// Задача 1
-// function checkAge(age) {
-//   return age > 18 || confirm('Родители разрешили?');
-// }
-
-// Задача 2
-// function min(a, b) {
-//   if (a < b) {
-//     return a;
-//   } else {
-//     return b;
+// function deleteMin(arr) {
+//   let accum = arr[0];
+//   for (i in arr) {
+//     if (arr[i] < accum) {
+//       accum = arr[i];
+//     }
 //   }
-// }
-
-// Задача 3
-// function pow(x, n) {
-//   let result = x;
-//   for (let i = 1; i < n; i++) {
-//     result *= x;
+//   for (i in arr) {
+//     if (arr[i] === accum) {
+//       arr.splice(i, 1);
+//     }
 //   }
-//   return result;
+//   return arr;
 // }
-// let x = prompt('x?', '');
-// let n = prompt('n?', '');
-// if (n < 1) {
-//   alert(`Степень ${n} не поддерживается, используйте натуральное число`);
-// } else {
-//   alert(pow(x, n));
-// }
+// console.log(deleteMin([-1, 3, 2, 4, 5, 6, -1, -1]));
+// console.log(deleteMin([-5, -6, -3, -2, -6, -1, -4, -6]));
+// console.log(deleteMin([1, 2, 3, 4, 5]));
 
-// 2.17 Стрелочные функции, основы
+// Задание 7*. Минимально возможная сумма
+// На входе мы мы получаем массив X положительных целых чисел, его элементы должны быть преобразованы путем
+// выполнения следующей операции над ними столько раз, сколько требуется:
+// if X[i] > X[j] then X[i] = X[i] - X[j]
+// Тестовые данные:
+// [6, 9, 21]
+// Процесс решения:
+// [6, 9, 12] #-> X[2] = 21 - 9
+// [6, 9, 6] #-> X[2] = 12 - 6
+// [6, 3, 6] #-> X[1] = 9 - 6
+// [6, 3, 3] #-> X[2] = 6 - 3
+// [3, 3, 3] #-> X[1] = 6 - 3
+// Ожидаемые результат
+// 9
 
-// Задача 1
-// function ask(question, yes, no) {
-//   if (confirm(question)) yes();
-//   else no();
+// function zalupa(arr) {
+//   arr = arr.sort((a, b) => a - b);
+//   arr.splice(arr.length - 1, 1, arr[arr.length - 1] - arr[arr.length - 2]);
+//   console.log(arr);
+//   console.log(arr.reduce((acc, num) => acc + num, 0));
+//   if (arr[arr.length - 1] != arr[arr.length - 2]) {
+//     zalupa(arr);
+//   }
+//   return arr.reduce((acc, num) => acc + num, 0);
 // }
-// ask(
-//   'Вы согласны?',
-//   () => alert('Вы согласились.'),
-//   () => alert('Вы отменили выполнение.')
-// );
+// console.log(zalupa([6, 9, 21, 7, 9645, 42354235, 312]));
