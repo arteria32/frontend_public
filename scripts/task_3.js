11000c55a66bd7f4d45af1936aa1f308681a8bd4
ZakirovRifkat <zakirov2323rifkat@gmail.com>
2022-12-24 05:41:03 +0300

    fix

--- scripts/task_3.js
+++ scripts/task_3.js
@@ -4,4 +4,4 @@ let counter = 0;
 for (let num of arr) {
   if (num % 2 !== 0) counter++;
 }
-console.log(oddNumbersCounter);
+console.log(counter);
--- scripts/task_7.js
+++ scripts/task_7.js
@@ -1,7 +1,7 @@
 //Задание 7
 const sum = (arr) => arr.reduce((acc, num) => acc + num, 0);
 
-let x = [3, 7, 11],
+let x = [3, 6, 21],
   oldSum = sum(x),
   newSum,
   iter = 0,
@@ -9,8 +9,8 @@ let x = [3, 7, 11],
   j;
 do {
   if (iter != 0) oldSum = newSum;
-  for (let i = x.length - 1; i > 0; i--) {
-    let j = i - 1;
+  for (i = x.length - 1; i > 0; i--) {
+    j = i - 1;
     if (x[i] > x[j]) {
       x[i] = x[i] - x[j];
     }
