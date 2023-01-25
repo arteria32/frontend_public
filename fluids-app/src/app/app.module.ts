bd87a06e52d508294b4463828265bf85994cd144
alsu Bulatova <bulatovaalsu22@gmail.com>
2023-01-25 16:37:35 +0300

    add services

--- fluids-app/src/app/app.module.ts
+++ fluids-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { MainComponent } from './modules/main/main.component';
+import { ReservoirComponent } from './modules/main/reservoir/reservoir.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ReservoirComponent
   ],
   imports: [
     BrowserModule,
---
+++ fluids-app/src/app/services/borders.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+
+import * as val  from '../common-data/values';
+
+@Injectable({ providedIn: 'root' })
+export class Borderservice {
+    private currentAcceptableValue: number[] = [];
+
+    private minPossibleValue:number = val.minPossibleValue;
+    private maxPossibleValue:number = val.maxPossibleValue;
+    private minAcceptableValue:number = val.minAcceptableValue;
+    private maxAcceptableValue:number = val.maxAcceptableValue;
+
+    private inputMinValueMessage:string = val.inputMinValueMessage;
+    private inputMaxValueMessage:string = val.inputMaxValueMessage;
+    private exceededValueMessage:string = val.exceededValueMessage;
+
+    private inputValidationResult = val.inputValidationResult;
+
+  constructor() {
+    this.currentAcceptableValue = [val.minAcceptableValue, val.maxAcceptableValue];
+  }
+
+  changeValues() {
+    this.minAcceptableValue = this.getBoundaryValue(val.inputMinValueMessage);
+    this.maxAcceptableValue = this.getBoundaryValue(val.inputMaxValueMessage);
+  
+    while (this.minAcceptableValue > this.maxAcceptableValue) {
+      alert(this.exceededValueMessage);
+      this.minAcceptableValue = this.getBoundaryValue(this.inputMinValueMessage);
+      this.maxAcceptableValue = this.getBoundaryValue(this.inputMaxValueMessage);
+    }
+  
+  };
+  
+  validateInput(input:any){
+    if (isNaN(input) || input === "") {
+      return this.inputValidationResult.NaN;
+    }
+    let boundaryValue = parseInt(input);
+    if (boundaryValue < this.minPossibleValue || boundaryValue > this.maxPossibleValue) {
+      return this.inputValidationResult.Invalid;
+    }
+    return this.inputValidationResult.OK;
+  };
+  
+    showValidationError(validationResult:string)  {
+    switch (validationResult) {
+      case this.inputValidationResult.NaN:
+        alert("Введите число");
+        break;
+      case this.inputValidationResult.Invalid:
+        alert(
+          `Значение должно быть ≧ ${this.minPossibleValue} и ≦ ${this.maxPossibleValue}`
+        );
+        break;
+    }
+  };
+  
+  getBoundaryValue(inputValueMessage: string) {
+    let boundaryValueInput = prompt(inputValueMessage);
+    let validationResult = this.validateInput(boundaryValueInput);
+  
+    while (validationResult !== this.inputValidationResult.OK) {
+        this.showValidationError(validationResult);
+      boundaryValueInput = prompt(inputValueMessage);
+      validationResult = this.validateInput(boundaryValueInput);
+    }
+  
+    return parseInt(boundaryValueInput);
+  };
+
+    
+}
